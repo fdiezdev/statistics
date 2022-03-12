111e5a084d35dd92337daa314986eb707c7447bd
Francisco Diez <frandiezdev@gmail.com>
2022-03-12 16:33:28 -0300

    Added sorting to graphing display

--- u1/barras.py
+++ u1/barras.py
@@ -6,16 +6,39 @@ y_label = str(input("Y axis label: "))
 title = str(input("Graph title: "))
 
 v = {}
+v_percent = [0.0]*n
+s = []
+
 for i in range(n):
     var_name = str(input("Insert variable "+str(i)+" name: "))
     var_value = float(input("Insert variable "+str(i)+" value: "))
     v[var_name] = var_value
+    s.append(var_value)
 
 print(v)
 x = list(v.keys())
 y = list(v.values())
 
-plt.bar(x, y, color ='blue')
+total = 0
+
+for i in range(0,len(s)):
+    total = total + s[i]
+
+print(total)
+
+
+for i in range(0, len(v)):
+    var_percent = (s[i]/total)*100
+    print(var_percent)
+    v_percent[i] = var_percent
+
+v_percent.sort(reverse=True)
+x.sort(reverse=True)
+y.sort(reverse=True)
+
+print(v_percent)
+
+plt.bar(x, v_percent, color ='blue')
 plt.xlabel(())
 
 fig = plt.figure(figsize = (10, 5))
---
+++ u1/freq_table.py
@@ -0,0 +1,2 @@
+hola = {'Hola': 0, 'Hola2': 1}
+print(hola['Hola'])
